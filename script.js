703f84bf5bb7e2d3ca71a96a2da11568bd7cfe91
Manuhsm <manuelahsm167@gmail.com>
2025-09-04 21:54:56 -0300

    Javascript

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("Olá mundo!")
+// alert("Olá mundo!")
+// alert("Oi")
+// alert("Tudo bem?")
+// alert("Você vem sempre aqui?")
+
+// let idade 
+
+// idade=18
+// alert(idade)
+// idade = "melancia"
+// alert(idade)
+// idade = 18.1 
+
+const amor=  "meu primeiro amor"
+alert(amor)
+
+let amor2 = "meu segundo amor"
+alert(amor2)
+
+var antigao= "Nunca usar essa poioca"  
+alert(antigao)
+

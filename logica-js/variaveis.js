6a87abcd55c2b8714bb46c2e5d3d728c06344ca9
Manuhsm <manuelahsm167@gmail.com>
2025-09-09 22:15:05 -0300

    variaveis em Javascript

---
+++ logica-js/variaveis.js
@@ -0,0 +1,23 @@
+//Variavel com var 
+// var nome = "Manuela"
+// var nome = "Everton"
+// nome = "Everton"
+
+//Variavel com let 
+// let nome = "Luciano"
+// nome = "Everton"
+
+//Variavel declarada com const
+const nome = "Manuela" //string
+const sobrenome = "Santos" 
+const idade = 17 //number
+const masculino = true //boolean 
+
+//array
+const frutas = ["laranja" , "maça" , "banana"] 
+
+console.log(frutas[2])
+// console.log("Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos")
+
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
+

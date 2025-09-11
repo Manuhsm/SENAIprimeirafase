6c94ffd085d7735380c4601e5fc9b71145771c23
Manuhsm <manuelahsm167@gmail.com>
2025-09-10 22:19:30 -0300

    sq02

---
+++ sq02/ex04.js
@@ -0,0 +1,15 @@
+let numero
+numero = Number (prompt("Digite um número: "))
+if(numero %2 == 0){
+    alert("par")
+}else{
+    alert("ímpar")
+}
+
+
+// ler um número
+
+//  se o resto da divisão por 2 for 0// se o número for par 
+// informar que é par
+// senão
+// informar que é impar  

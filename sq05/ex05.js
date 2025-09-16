8329c83fa75aea385d97b325a646454ea47a968e
Manuhsm <manuelahsm167@gmail.com>
2025-09-15 22:03:42 -0300

    sq05

---
+++ sq05/ex05.js
@@ -0,0 +1,32 @@
+// Exercício 5.1
+
+// let senha
+
+// senha = prompt("Digite a senha:")
+// if(senha == 1234){
+//     alert("ACESSO PERMITIDO")
+// }else{
+//     alert("ACESSO NEGADO")
+// }
+
+//Exercícios 5.2
+let A,B
+
+A = Number(prompt("Digite o primeiro número:"))
+B = Number(prompt("Digite o segundo número:"))
+
+if(A>B){
+    alert("Primeiro ("+A+") é maior que o segundo ("+B+")")
+}else if(B>A){
+    alert("Segundo ("+B+") é maior que o primeiro ("+A+")")
+}else{
+    alert("São iguais!!!!!")
+}
+
+//Exercícios 5.3
+
+
+
+
+
+

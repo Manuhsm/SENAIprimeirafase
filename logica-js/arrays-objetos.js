b5cf492c0ecf48fa0d72176cd4567546c8ecde2d
Manuhsm <manuelahsm167@gmail.com>
2025-09-16 22:13:17 -0300

    logica-js 2

---
+++ logica-js/arrays-objetos.js
@@ -0,0 +1,25 @@
+const alunosDoSenai = [
+    {
+        nome:"Evertizinho",
+        email:"evertilizinho@gmail.com",
+        matricula:"123",
+        idade:19
+    },
+    {
+        nome:"Manuela",
+        email:"manu@gmail.com",
+        matricula:"2237",
+        idade:18
+    },
+    {
+        nome:"Maya Maria",
+        email:"euamopetiscos@gmail.com",
+        matricula:"2502",
+        idade:2
+    }
+]
+
+console.log(`Meu nome é ${alunosDoSenai[2].nome} 
+    meu email é ${alunosDoSenai[2].email} 
+    minha matricula é ${alunosDoSenai[2].matricula}
+     e minha idade é ${alunosDoSenai[2].idade}`)

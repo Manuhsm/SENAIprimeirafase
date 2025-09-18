73e5f0add44f037368ececc56251650b8e4f33a9
Manuhsm <manuelahsm167@gmail.com>
2025-09-17 21:59:08 -0300

    sq05

--- sq05/ex05.js
+++ sq05/ex05.js
@@ -10,23 +10,54 @@
 // }
 
 //Exercícios 5.2
-let A,B
 
-A = Number(prompt("Digite o primeiro número:"))
-B = Number(prompt("Digite o segundo número:"))
+// let A,B
 
-if(A>B){
-    alert("Primeiro ("+A+") é maior que o segundo ("+B+")")
-}else if(B>A){
-    alert("Segundo ("+B+") é maior que o primeiro ("+A+")")
-}else{
-    alert("São iguais!!!!!")
-}
+// A = Number(prompt("Digite o primeiro número:"))
+// B = Number(prompt("Digite o segundo número:"))
+
+// if(A>B){
+//     alert("Primeiro ("+A+") é maior que o segundo ("+B+")")
+// }else if(B>A){
+//     alert("Segundo ("+B+") é maior que o primeiro ("+A+")")
+// }else{
+//     alert("São iguais!!!!!")
+// }
 
 //Exercícios 5.3
 
+// let placa 
+// placa = Number(prompt("Ùltimo número:"))
+// if(placa == 0 || placa == 1 ){
+//     alert("Não pode rodar na segunda")
+// }else if(placa == 2 || placa == 3){
+//     alert("Não pode rodar na terça")
+// }else if(placa == 4 || placa == 5){
+//     alert("Não pode rodar na quarta")
+// }else if(placa == 6 || placa == 7){
+//     alert("Não pode rodar na quinta")
+// }else if(placa == 8 || placa == 9){
+//     alert("Não pode rodar na sexta")
+// }
 
+//Exercícios 5.4
 
+let palestra 
+palestra = Number(prompt("Número da palestra:"))
+if(palestra == 1){
+    alert("Animações em Scratch, laboratório 305, 19h")
+}else if(palestra == 2 ){
+    alert("Scratch para gamers, laboratório 512, 20h")
+}else if(palestra == 3 ){
+    alert("JavaScript para leigos, laboratório 101, 19h")
+}else if(palestra == 4 ){
+    alert("Tópios avançados de JavaScript, laboratório 305, 20h")
+}else if(palestra == 5){
+    alert("Vida e carreira, auditório, 21h")
+}
 
+//Exercício 5.5
+
+let
 
 

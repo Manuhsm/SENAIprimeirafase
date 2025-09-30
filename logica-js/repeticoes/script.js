c4406221fe58ea15b504ec05e539b585bb4e60a8
Manuhsm <manuelahsm167@gmail.com>
2025-09-29 22:16:06 -0300

    repetições

---
+++ logica-js/repeticoes/script.js
@@ -0,0 +1,11 @@
+// let acertos = Number(prompt("Quantos acertos?"))
+// let valorPorQuestão = 10/16 
+// let nota = acertos * valorPorQuestão
+
+// alert("Sua nota : " + nota)
+
+let contador = 0
+while(contador < 5){
+   alert("Contador :"+ contador)
+   contador = contador + 1
+}

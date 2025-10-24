b3c027a077c98605cec8abbb78dd5fe03a8f255e
Manuhsm <manuelahsm167@gmail.com>
2025-10-23 22:18:03 -0300

    Dom revisão

---
+++ Dom-manipulação/dom-revisão/script.js
@@ -0,0 +1,20 @@
+// let usuario
+// function cadastrarUsuario(){
+//     // console.log("até aqui deu");
+//     usuario = document.getElementById("inputUsuario").value 
+
+//     document.getElementById("inputUsuario").value =''
+
+//     document.getElementById("resultado").innerHTML = "Usuario cadastrado com sucesso: " + usuario
+// }
+
+let x=500
+let y=35.0
+let media
+function totalDeCombustivel(){
+    media= x/y
+     document.getElementById
+
+}
+
+
